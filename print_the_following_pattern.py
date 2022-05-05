0c0f3995467d1e3d6c0767839414259f4cf883d2
ParameshPK81 <97424433+ParameshPK81@users.noreply.github.com>
2022-05-05 11:15:13 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n+1):         if i==j:             print('0',end='')         else:             print('x',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,9 @@
 n=int(input())
-for j in range(1,n+1):
-    for i in range(1,j+1):
-        print(i,end='')
-    print()
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j:
+            print('0',end='')
+        else:
+            print('x',end='')
+    print()        
+            
