92946aa10c8c9b8c5f635c88451ef26705cf15b2
ParameshPK81 <97424433+ParameshPK81@users.noreply.github.com>
2022-05-19 11:57:30 +0530

    def crd(n):     c=[0]*10     res=0     while n>0:         d=n%10         c[d]+=1         n=n//10     for i in range(10) :         if(c[i]>1):             res+=1     return res n=int(input()) res=crd(n) if res>0:     print('Not Unique Number') else:     print('Unique Number');

---
+++ Unique_number.py
@@ -0,0 +1,18 @@
+def crd(n):
+    c=[0]*10
+    res=0
+    while n>0:
+        d=n%10
+        c[d]+=1
+        n=n//10
+    for i in range(10) :
+        if(c[i]>1):
+            res+=1
+    return res            
+n=int(input())            
+res=crd(n)
+if res>0:
+    print('Not Unique Number')
+else:
+    print('Unique Number');
+    
