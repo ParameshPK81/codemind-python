665da1566ce0667614bf794c026fd5064397f504
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-22 23:15:34 +0530

    h,sp,s=map(int,input().split()) if h>=50 and sp>=60 and s>=100:     print(10) elif h>=50 and sp>=60 and s<=100:     print(9) elif h<=50 and sp>=60 and s>=100:     print(8) elif h>=50 and sp<=60 and s>=100:     print(7) elif h<=50 and sp<=60 and s<=100:     print(5) else:     print(6)

---
+++ Grades_of_Rides.py
@@ -0,0 +1,13 @@
+h,sp,s=map(int,input().split())
+if h>=50 and sp>=60 and s>=100:
+    print(10)
+elif h>=50 and sp>=60 and s<=100:
+    print(9)
+elif h<=50 and sp>=60 and s>=100:
+    print(8)
+elif h>=50 and sp<=60 and s>=100:
+    print(7)    
+elif h<=50 and sp<=60 and s<=100:
+    print(5)
+else:
+    print(6)
