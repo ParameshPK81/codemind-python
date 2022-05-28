3ba977f8c12f04e6476d81b08941a3cf7f0a32cc
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-28 11:12:53 +0530

    n=int(input()) f=0 if n>0:     while n>0:         if n%2==0:#6             n//=2#3         elif n%3==0:#3             n//=3#1         elif n%5==0:             n//=5         else:             f=1             break if f==1:     if n==1:         print('Ugly Number')     else:         print('Not Ugly Number') else:     print('Not Ugly Number')

--- Ugly_number.py
+++ Ugly_number.py
@@ -2,10 +2,10 @@ n=int(input())
 f=0
 if n>0:
     while n>0:
-        if n%2==0:
-            n//=2
-        elif n%3==0:
-            n//=3
+        if n%2==0:#6
+            n//=2#3
+        elif n%3==0:#3
+            n//=3#1
         elif n%5==0:
             n//=5
         else:
