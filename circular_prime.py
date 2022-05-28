2cffe69090d85771344c1c763ca806f914bb5703
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-28 21:34:33 +0530

    def fun(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) rem=0 if fun(n):     while(n):         d=n%10         n=n//10         rem=rem*10+d     if fun(rem):         print('circular prime')     else:         print('prime but not a circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,17 +1,17 @@
-def prime(n):
-    for i in range(2,int((n**0.5)+1)):
+def fun(n):
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
-            return 0
+            return False
     else:
-        return 1
+        return True
 n=int(input())
-r=0
-if prime(n):
-    while n>0:
+rem=0
+if fun(n):
+    while(n):
         d=n%10
         n=n//10
-        r=r*10+d
-    if prime(r):
+        rem=rem*10+d
+    if fun(rem):
         print('circular prime')
     else:
         print('prime but not a circular prime')
