315dba336a461d38deb1b91e8d3d1166261dfbf7
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-06-30 09:53:02 +0530

    n=int(input()) n=str(n) c=0 for i in n:     if n.count(i)>1:         c+=1 if c==0:     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,17 +1,10 @@
-def crd(n):
-    c=[0]*10
-    res=0
-    while n>0:
-        d=n%10
-        c[d]+=1
-        n=n//10
-    for i in range(10) :
-        if(c[i]>1):
-            res+=1
-    return res            
-n=int(input())            
-res=crd(n)
-if res>0:
-    print('Not Unique Number')
+n=int(input())
+n=str(n)
+c=0
+for i in n:
+    if n.count(i)>1:
+        c+=1
+if c==0:
+    print("Unique Number")
 else:
-    print('Unique Number')
+    print("Not Unique Number")
