45f2d57bcd3bc5cba2ccb084e366baa123ba23ba
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-01 10:36:48 +0530

    def gcd(a,b):     if(b==0):         return a     return gcd(b,a%b) a,b=map(int,input().split()) print(gcd(a,b))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,11 +1,6 @@
 def gcd(a,b):
-    if a>b:
-        a,b=b,a
-    c=a
-    while True:
-        if a%c==0 and b%c==0:
-            return c
-        c-=1
-
-n,m=map(int,input().split())
-print(gcd(n,m))
+    if(b==0):
+        return a
+    return gcd(b,a%b)
+a,b=map(int,input().split())
+print(gcd(a,b))
