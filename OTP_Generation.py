da172aa707f6db69f79c5ca3d5cc2f651ecc8d5f
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-17 11:25:17 +0530

    n=int(input()) x=0 while(n):     j=n%10     x=x*10+j     n=n//10 while(x):     i=x%10     x=x//10     if(i%2==0):         continue     else:         k=i*i         print(k,end='')

---
+++ OTP_Generation.py
@@ -0,0 +1,14 @@
+n=int(input())
+x=0
+while(n):
+    j=n%10
+    x=x*10+j
+    n=n//10
+while(x):
+    i=x%10
+    x=x//10
+    if(i%2==0):
+        continue
+    else:
+        k=i*i
+        print(k,end='')
