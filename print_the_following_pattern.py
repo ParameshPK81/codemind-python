5187ee2ec968c7e3f965b4aa95a469305e757de7
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-17 15:15:58 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n+1):         if i==j or j==n+1-i:             print('x',end='')         else:             print(0,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,8 @@
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if j==1 or j==n or i==j:
-            print("*",end=' ')
+        if i==j or j==n+1-i:
+            print('x',end='')
         else:
-            print(' ',end=' ')
+            print(0,end='')
     print()
