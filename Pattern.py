1bdd20ca0905037ac08305880ff22cc7b2f3c9ad
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-17 16:06:40 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(n,i,-1):         print(' ',end='')     for k in range(1,i+1):         print(k,end='')     for I in range(i-1,0,-1):         print(I,end='')     print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,9 @@
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=' ')
+    for j in range(n,i,-1):
+        print(' ',end='')
+    for k in range(1,i+1):
+        print(k,end='')
+    for I in range(i-1,0,-1):
+        print(I,end='')
     print()
