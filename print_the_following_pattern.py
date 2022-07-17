5a190122d14ea75b29d6fca662dda424864c6024
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-17 15:45:28 +0530

    n=int(input()) for i in range(1,n+1,1):     for j in range(n,i,-1):         print(' ',end='')     for k in range(i,0,-1):         print(k-1,end='')     for I in range(1,i,1):         print(I,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,9 +2,8 @@ n=int(input())
 for i in range(1,n+1,1):
     for j in range(n,i,-1):
         print(' ',end='')
-    for k in range(1,i+1,1):
-        print(i,end='')
+    for k in range(i,0,-1):
+        print(k-1,end='')
     for I in range(1,i,1):
-        print(i,end='')
+        print(I,end='')
     print()
-        
