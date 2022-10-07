4a09c8c7cfe1e2dcd414e7bdfa919504c3634c1a
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-10-07 12:56:14 +0530

    t=int(input()) for i in range(t):     x,y=map(int,input().split())     a=list(map(int,input().split()))     b=list(map(int,input().split()))     c=a+b     c.sort()     print(*c)

---
+++ Merge_Without_Extra_Space.py
@@ -0,0 +1,8 @@
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    c=a+b
+    c.sort()
+    print(*c)
