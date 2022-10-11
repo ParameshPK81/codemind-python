2eca5cf9f414c31be71ed68d832c8417ec9cf8ed
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-10-11 23:18:13 +0530

    n=int(input()) a=list(map(int,input().split())) for i in a:     print(len(str(abs(i))),end=' ')

--- count_digits.py
+++ count_digits.py
@@ -1,18 +1,4 @@
-'''def count(n):
-    c=0
-    if n<0 or n>0:
-        n=abs(n)
-        while n>=0:
-            d=n%10
-            n//=10
-            c+=1
-    if n==0:
-        c=1
-    return c
-    '''
 n=int(input())
 a=list(map(int,input().split()))
 for i in a:
-    i=abs(i)
-    i=str(i)
-    print(len(i),end=" ")
+    print(len(str(abs(i))),end=' ')
