dc9befc39ea7ed3b91dc7370d41a3c6b4760c05d
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-10-11 07:46:06 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() a=[] for i in s1:     if i==' ':         continue     if i not in s2 and i not in a:         a.append(i) for i in s2:     if i not in s1 and i not in a:         a.append(i) a.sort() b='' for i in a:     b+=i print(b)

---
+++ non_common_characters.py
@@ -0,0 +1,18 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+a=[]
+for i in s1:
+    if i==' ':
+        continue
+    if i not in s2 and i not in a:
+        a.append(i)
+for i in s2:
+    if i not in s1 and i not in a:
+        a.append(i)
+a.sort()
+b=''
+for i in a:
+    b+=i
+print(b)
